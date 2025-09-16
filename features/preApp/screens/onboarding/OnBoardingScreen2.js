a24d8262a711301a0e1e35b3aa3b5948ec22f7e9
AntonKenderov <anton.kenderov1@gmail.com>
2025-09-16 19:31:08 +0300

    Created file for second OnBoarding Screen

---
+++ features/preApp/screens/onboarding/OnBoardingScreen2.js
@@ -0,0 +1,12 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+
+export default function OnBoardingScreen2() {
+    return(
+        <>
+        
+        
+        
+        </>
+    )
+}
